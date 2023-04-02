2abf08be97f4a02e23aaf47879f25ccbf4aadffa
Samso Supriyatna <semzthespider@gmail.com>
2023-04-02 07:15:38 +0700

    Starter Commit

---
+++ app/Database/Migrations/2023-04-01-080830_CreateUsersTable.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreateUsersTable extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type' => 'INT',
+                'constraint' => 15,
+                'unsigned' => true,
+            ],
+            'email' => [
+                'type' => 'VARCHAR',
+                'constraint' => 255,
+            ],
+            'password' => [
+                'type' => 'VARCHAR',
+                'constraint' => 255,
+            ],
+            'created_at datetime default current_timestamp',
+            'updated_at datetime default current_timestamp on update current_timestamp'
+        ]);
+        $this->forge->addPrimaryKey('id');
+        $this->forge->createTable('users');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('users');
+    }
+}
